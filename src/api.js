b51b7435097d8ce661d4348fdadcd1adb4ff2b84
Jonah Williams <jonahwilliams@Jonahs-MacBook-Pro.local>
2015-10-06 17:26:20 -0700

    fixed api connection to locahost

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const get = (date, rent) => {
   return new Promise((resolve, reject) => {
     let req = new XMLHttpRequest();
-    req.open('GET', `http://localhost:8080/rent/${date}/${rent}`);
+    req.open('GET', `/rent/${date}/${rent}`);
     req.onload = () => {
       if (req.status == 200) {
         resolve(req.response);
